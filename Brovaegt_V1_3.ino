7f936b6619b467a20e18bccd56973ff32c7f16f1
mikael1982-lang <mikael1982@gmail.com>
2026-08-08 09:28:27 +0200

    Test R200 single polling

--- Brovaegt_V1_3.ino
+++ Brovaegt_V1_3.ino
@@ -9,6 +9,7 @@
 const int RFID_RX_PIN = 16;
 const int RFID_TX_PIN = 17;
 const byte READ_MULTI_CMD[10] = {0xAA, 0x00, 0x27, 0x00, 0x03, 0x22, 0xFF, 0xFF, 0x4A, 0xDD};
+const byte SINGLE_POLL_CMD[7] = {0xAA, 0x00, 0x22, 0x00, 0x00, 0x22, 0xDD};
 const size_t RFID_MAX_FRAME_LENGTH=64;
 const unsigned long RFID_TAG_TIMEOUT=1000;
 
@@ -168,7 +169,7 @@ void setup(){
   tare();
 
   Serial2.begin(115200,SERIAL_8N1,RFID_RX_PIN,RFID_TX_PIN);
-  Serial2.write(READ_MULTI_CMD,sizeof(READ_MULTI_CMD));
+  Serial2.write(SINGLE_POLL_CMD,sizeof(SINGLE_POLL_CMD));
 
   WiFi.mode(WIFI_AP);
   WiFi.softAP(ssid,password);
